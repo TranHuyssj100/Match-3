f1c7a7bba6ff461a82b75e42993d0adaa7e44ed2
tranlew <tranhuyssj100@gmail.com>
2026-09-15 11:33:40 +0700

    Cache Value

--- Assets/Scripts/Utilities/Utils.cs
+++ Assets/Scripts/Utilities/Utils.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,21 +6,31 @@ using URandom = UnityEngine.Random;
 
 public class Utils
 {
+    private static readonly NormalItem.eNormalType[] m_normalTypes =
+        (NormalItem.eNormalType[])Enum.GetValues(typeof(NormalItem.eNormalType));
+
+    private static readonly List<NormalItem.eNormalType> m_allowedTypes =
+        new List<NormalItem.eNormalType>(m_normalTypes.Length);
+
     public static NormalItem.eNormalType GetRandomNormalType()
     {
-        Array values = Enum.GetValues(typeof(NormalItem.eNormalType));
-        NormalItem.eNormalType result = (NormalItem.eNormalType)values.GetValue(URandom.Range(0, values.Length));
-
-        return result;
+        return m_normalTypes[URandom.Range(0, m_normalTypes.Length)];
     }
 
-    public static NormalItem.eNormalType GetRandomNormalTypeExcept(NormalItem.eNormalType[] types)
+    public static NormalItem.eNormalType GetRandomNormalTypeExcept(List<NormalItem.eNormalType> types)
     {
-        List<NormalItem.eNormalType> list = Enum.GetValues(typeof(NormalItem.eNormalType)).Cast<NormalItem.eNormalType>().Except(types).ToList();
+        if (types == null || types.Count == 0) return GetRandomNormalType();
 
-        int rnd = URandom.Range(0, list.Count);
-        NormalItem.eNormalType result = list[rnd];
+        m_allowedTypes.Clear();
+        for (int i = 0; i < m_normalTypes.Length; i++)
+        {
+            if (types.Contains(m_normalTypes[i])) continue;
 
-        return result;
+            m_allowedTypes.Add(m_normalTypes[i]);
+        }
+
+        if (m_allowedTypes.Count == 0) return GetRandomNormalType();
+
+        return m_allowedTypes[URandom.Range(0, m_allowedTypes.Count)];
     }
 }
